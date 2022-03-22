6153ace0f88b3304b34061fea4d326ebd7971425
Tomsanchis <92373414+Tomsanchis@users.noreply.github.com>
2022-03-22 11:39:39 +0100

    hobbies object create

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,28 @@
 import { v4 as uuidv4 } from 'uuid';
 
+const hobbies = [
+    {id: 1, name: "foot"},
+    {id: 2, name: "voley"},
+    {id: 3, name: "tenis"},
+    {id: 4, name: "drink"},
+    {id: 5, name: "eat"},
+    {id: 6, name: "run"},
+    {id: 7, name: "cooking"},
+    {id: 8, name: "vtt"},
+    {id: 9, name: "motocross"},
+    {id: 10, name: "mechanic"},
+    {id: 11, name: "devlopment web"},
+    {id: 12, name: "video games"},
+    {id: 13, name: "kart"},
+    {id: 14, name: "escalade"},
+    {id: 15, name: "look the tv"},
+    {id: 16, name: "draw"},
+    {id: 17, name: "randonnée"},
+    {id: 18, name: "ski"},
+    {id: 19, name: "camping"},
+    {id: 20, name: "box"},
+]
+
 function randomid () {
     let tempid = []
     for (let i = 0; i < 10; i++) {
